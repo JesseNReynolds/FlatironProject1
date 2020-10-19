251af86aa7de17f448ec0dee9068d0a5ab31f71c
JesseNReynolds <jessenreynolds@gmail.com>
2020-10-19 17:50:12 -0600

    Cleaned up structure/class names.

--- lib/initial_query.rb
+++ lib/initial_query.rb
@@ -2,9 +2,13 @@
 # and returning an array of businesses. It then creates an array of
 # restaurant objects to be filtered and sampled.
 
+require 'open-uri'
+require 'net/http'
+require 'json'
 
 class InitialQuery
-
+    attr_reader :limit, :category, :longitude, :latitude,
+        :radius, :parsed_hash
 
 
     def initialize(longitude, latitude, radius)
@@ -15,8 +19,17 @@ class InitialQuery
         @radius = radius
     end
 
-    def query
-        url = "https://api.yelp.com/v3/businesses/search?limit=#{@limit}&category=#{@category}&latitude=#{@latitude}&longitude=#{@longitude}&radius=#{@radius}"
-        uri = URI.parse(url)
+    def query_to_hash
+        uri = "https://api.yelp.com/v3/businesses/search?limit=#{@limit}&category=#{@category}&latitude=#{@latitude}&longitude=#{@longitude}&radius=#{@radius}"
+        raw_query_data = URI.parse(url)
+        @parsed_hash = JSON.parse(raw_query_data)
+    end
+
+    def hash_to_objects
+
+
+    end
+
+
 
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -2,20 +2,27 @@ require_relative '../lib/initial_query'
 
 
 class Logic
+    attr_reader :longitude, :latitude, :radius, :query
 
     def start
-        puts "Please enter longitude:"
-        longitude = gets.chomp
+        initial_inputs
 
-        puts "Please enter latitude:"
-        latitude = gets.chomp
 
-        puts "enter radius (in miles) for search"
-        radius = (gets.chomp * 1609.34) 
-        # This converts from miles, the standard unit of travel distance in the US, to meters, the unit that the Yelp API uses.
-       
-        InitialQuery.new(longitude, latitude, radius)
+
     end
 
+    def initial_inputs
+        puts "Please enter longitude:"
+        @longitude = gets.chomp
+
+        puts "Please enter latitude:"
+        @latitude = gets.chomp
+
+        puts "enter radius (in miles) for search"
+        @radius = (gets.chomp * 1609.34) 
+        # This converts from miles, the standard unit of travel distance in the US, to meters, the unit that the Yelp API uses.
+       
+        @query = InitialQuery.new(@longitude, @latitude, @radius)
+    end
 
 end
