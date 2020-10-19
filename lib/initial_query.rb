a4a19698f1bf57cceade2fcd45dbd9299ae31e06
JesseNReynolds <jessenreynolds@gmail.com>
2020-10-19 17:33:52 -0600

    Added liscense, refactored for longitude/latitude to support non US users and more specific results.

--- lib/initial_query.rb
+++ lib/initial_query.rb
@@ -1,12 +1,22 @@
+# This class is responsible for formatting the initial query to the API
+# and returning an array of businesses. It then creates an array of
+# restaurant objects to be filtered and sampled.
+
+
 class InitialQuery
 
-"https://api.yelp.com/v3/businesses/search?limit=#{@limit}&category=#{@category}&location=#{@location}&radius=#{@radius}"
 
-    def initialize(location,radius)
+
+    def initialize(longitude, latitude, radius)
         @limit = 50
         @category = "Restaurants"
-        @location = location
+        @longitude = longitude
+        @latitude = latitude
         @radius = radius
     end
+
+    def query
+        url = "https://api.yelp.com/v3/businesses/search?limit=#{@limit}&category=#{@category}&latitude=#{@latitude}&longitude=#{@longitude}&radius=#{@radius}"
+        uri = URI.parse(url)
 
 end
--- lib/run_stucture.rb
+++ lib/run_stucture.rb
@@ -1,11 +1,21 @@
+require_relative '../lib/initial_query'
+
+
 class Logic
 
     def start
-        puts "enter zip code in which to search"
-        zip = gets.chomp
+        puts "Please enter longitude:"
+        longitude = gets.chomp
+
+        puts "Please enter latitude:"
+        latitude = gets.chomp
+
         puts "enter radius (in miles) for search"
-        radius = (gets.chomp / 1609.34)
-        InitialQuery.new(zip, radius)
+        radius = (gets.chomp * 1609.34) 
+        # This converts from miles, the standard unit of travel distance in the US, to meters, the unit that the Yelp API uses.
+       
+        InitialQuery.new(longitude, latitude, radius)
     end
+
 
 end
